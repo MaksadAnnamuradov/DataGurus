8dfe8bd8437d5e039621f7f0fda39711896d2d5b
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-03-03 22:50:54 -0700

    playing with styling

--- app/dash/file_uploader.py
+++ app/dash/file_uploader.py
@@ -10,43 +10,19 @@ import os
 from io import StringIO
 from .dash import Dash
 
-# cwd = os.getcwd()
-# UPLOAD_FOLDER = cwd + '\\www'
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-# UPLOAD_FOLDER ='C:/Users/maksa/Documents/projects/DataGurus'
 
 app_layout = html.Div( 
         children=[
             html.H1("Upload File"),
-            html.Iframe(id='iframe-upload',src=f'/upload', height='500', width='500'),
+            html.Iframe(id='iframe-upload',src=f'/upload', height='300', width='300'),
             html.Div(id='output')
                 ]
 )
 
-# @app.server.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         filename = secure_filename(file.filename)
-
-#         df = pd.read_csv(request.files['file'].stream._file, encoding='shift-jis')
-
-#         print(df)
-#         #file.save(os.path.join(UPLOAD_FOLDER, filename))
-
-#     return '''
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
-
 
 def init_dash(flask_server):
-    stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     """Create a Plotly Dash dashboard."""
-    dash_app = Dash(server=flask_server, routes_pathname_prefix="/file/", external_stylesheets=stylesheets)
+    dash_app = Dash(server=flask_server, routes_pathname_prefix="/file/")
 
     # create dash layout
     dash_app.layout = app_layout
--- app/dash/mongo_dash_datatable.py
+++ app/dash/mongo_dash_datatable.py
@@ -113,11 +113,7 @@ def parse_contents(contents, filename, date):
 
     return html.Div([
         html.H5(filename),
-        # dash_table.DataTable(
-        #     data=df.to_dict('records'),
-        #     columns=[{'name': i, 'id': i} for i in df.columns],
-        #     page_size=15
-        # ),
+     
          dash_table.DataTable(
             id='my-table',
             columns=[{
@@ -151,19 +147,6 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             parse_contents(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
-
-# def import_content(filepath):
-#     mng_client = pymongo.MongoClient('localhost', 27017)
-#     mng_db = mng_client['mongodb_name'] // Replace mongo db name
-#     collection_name = 'collection_name' // Replace mongo db collection name
-#     db_cm = mng_db[collection_name]
-#     cdir = os.path.dirname(__file__)
-#     file_res = os.path.join(cdir, filepath)
-
-#     data = pd.read_csv(file_res)
-#     data_json = json.loads(data.to_json(orient='records'))
-#     db_cm.remove()
-#     db_cm.insert(data_json)
 
 # Display Datatable with data from Mongo database *************************
 @app.callback(Output('mongo-datatable', 'children'),
